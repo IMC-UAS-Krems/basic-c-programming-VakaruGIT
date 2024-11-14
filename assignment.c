af2379b5fecb68be39d90e82da54a5be9333364b
Andrei-Flavius Văcaru <andreivacaru2002@yahoo.com>
2024-11-14 12:45:51 +0100

    Added conditions to whitespace, fixed extra newline if no more rows

--- assignment.c
+++ assignment.c
@@ -28,8 +28,8 @@ int main(int argc, char *argv[]) {
     int cols = atoi(argv[2]);
 
     // Dynamically allocate memory for the matrix
-    int **matrix = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
+    int **matrix = (int **)malloc(rows * sizeof(int *)); // **matrix is a pointer to a pointer
+    for (int i = 0; i < rows; i++) { 
         matrix[i] = (int *)malloc(cols * sizeof(int));
         for (int j = 0; j < cols; j++) {
             matrix[i][j] = rand() % (maxrand - minrand + 1) + minrand;
@@ -38,26 +38,34 @@ int main(int argc, char *argv[]) {
 
     // Open file for writing
     FILE *file = fopen("matrix.txt", "w");
-    if (!file) {
-        printf("Failed opening the file\n");
+    if (!file) { // if file does not exist
+        printf("Failed opening the file");
         return 1;
     }
 
     // Write matrix to file
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            fprintf(file, "%d ", matrix[i][j]); // fprintf() is used to write output to file
+            if (j == cols - 1) {
+                fprintf(file, "%d", matrix[i][j]); // fprintf() writes formatted output to file
+            }
+            else{
+                fprintf(file, "%d ", matrix[i][j]); 
+            }
+
         }
+    if (i != rows - 1){ // add the newline only if it is not the last row
         fprintf(file, "\n");
     }
-
+    }
     fclose(file);
 
-    // Free memory 
+    // Free memory (good practice because we allocated memory)
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
 
     return 0;
+    // i tried the code with the arguments 5 5
 }
