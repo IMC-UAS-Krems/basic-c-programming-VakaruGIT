619a4ee67eb51bd46976397dec866f2986e775e4
Andrei-Flavius Văcaru <andreivacaru2002@yahoo.com>
2024-11-14 10:33:44 +0100

    Modified printf for incorrect nr of arguments

--- assignment.c
+++ assignment.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     // WRITE YOUR CODE HERE
     // Check if number of arguments is different from 2 // argc is the number of arguments
     if (argc != 3) {
-        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc);
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc - 1);
         return 1;
     }
     // Arguments need to be integers greater than 0 // atoi() is used to convert string to integer
